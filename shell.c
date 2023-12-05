0727adeb0ba80ef9e6a69e272becba4e3539a28d
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-04 18:28:31 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -12,3 +12,12 @@ void prompt(void)
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, str, 2);
 }
+
+/**
+ * read_input - gets and parses a line from the prompt
+ * @str: line to parse
+ *
+ * Return: Array of tokens to be executed
+ */
+char **read_input(char *str)
+{
