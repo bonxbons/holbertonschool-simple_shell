2e21c61929fb53e4e3a3d939d4365f7b91c9c1da
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-05 07:52:13 -0800

    typo

--- strings.c
+++ strings.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 /**
- * strlen - gives the length of the string
+ * _strlen - gives the length of the string
  * @s: the pointer to the string
  * Return: the length of string
  */
-int strlen(char *s)
+int _strlen(char *s)
 {
 	int i = 0;
 
