ac22ac01cc28337c612a095da46a0f5eaab16178
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-04 18:56:18 -0600

    fixing mistake

--- shell.c
+++ shell.c
@@ -65,13 +65,10 @@ void execute(char **commands, char *buffer, char **env, char **argv, int count)
 
 	if (commands == NULL)
 		null_command(buffer);
-	/*check if command is exit to exit from shell*/
 	else if (_strcmp("exit", commands[0]))
 		get_out(buffer, commands);
-	/* check if the command is ENV to print environment variables */
 	else if (_strcmp("env", commands[0]))
 		env_end(buffer, commands, env);
-	/*check if the command is a full path to an executable file*/
 	else if (stat(commands[0], &fileStat) == 0)
 		execve(commands[0], commands, NULL);
 	
