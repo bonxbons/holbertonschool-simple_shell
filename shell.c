ca5493b40ee847ea79e4f4bd73b194726ee24b13
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-05 08:14:07 -0800

    fixing whitespace

--- shell.c
+++ shell.c
@@ -21,7 +21,7 @@ void prompt(void)
  */
 char **read_input(char *str)
 {
-        char *token, **holder;
+	char *token, **holder;
 	unsigned int length;
 	int i = 0;
 
@@ -34,7 +34,7 @@ char **read_input(char *str)
 		return (NULL);
 	token = strtok(str, " ");
 	while (token != NULL)
-	{       
+	{      
 		holder[i] = malloc(_strlen(token) + 1);
 		if (holder[i] == NULL)
 		{
