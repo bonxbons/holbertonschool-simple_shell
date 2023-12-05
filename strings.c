1842c3fb027ed0f7fabf441e3bfc90613c0d7f52
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-05 08:36:51 -0800

    adding a string function

--- strings.c
+++ strings.c
@@ -14,3 +14,22 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
+/**
+ * _strcmp - compares two strings and find if are equal
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: 1 for equal, or 0 if not
+ */
+int _strcmp(char *s1, char *s2)
+{
+	unsigned int i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
