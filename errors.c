4738e566a264664adc64455b497381020a4a7a0a
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-06 14:42:12 -0800

    writing error commands

---
+++ errors.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * fork_fail - function that will handle for fail
+ * return: nothing
+ */
+void fork_fail(void)
+{
+	perror("Failed: ");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * build_message - a function to write and error message
+ * @av: argument vector
+ * @first_com: first command to print if not found
+ * @count: the number of times executed
+ * return: nothing
+ */
+void build_message(char **av, char *first_com, int count)
+{
+	int num = 1, cp, mult = 1;
+
+	write(STDERR_FILENO, av[0], _strlen(av[0]));
+	write(STDERR_FILENO, ":", 2);
+	cp = count;
+
+	while (cp >= 10)
+	{
+		cp /= 10;
+		mult *= 10;
+		num++;
+	}
+
+	while (num > 1)
+	{
+		if ((count / mult) < 10)
+			_puterror((count / mult + '0'));
+		else
+			_puterror((count / mult) % 10 + '0');
+		--num;
+		mult /= 10;
+	}
+
+	_puterror(count % 10 + '0');
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, first_com, _strlen(first_com));
+	wrtie(STDERR_FILENO, ": not found\n", 12);
+}
