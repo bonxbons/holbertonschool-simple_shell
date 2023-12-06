89f8f25cb03b64e6f4b004d6c99ada9bb64ff7a5
Jordan <jordan.arguelles.ja@gmail.com>
2023-12-06 14:27:08 -0800

    adding a string function

--- strings.c
+++ strings.c
@@ -69,3 +69,23 @@ unsigned int _strlen_const(const char *str)
 	return (i);
 
 }
+
+/**
+ * _strncpy - function that will copy a string into other
+ * @dest: destination of the string
+ * @src: the string to copy
+ * @n: length of the string
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0, i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
