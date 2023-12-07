9cd03dea224e3a0a7207c5a9347e9487986af3bb
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-07 15:05:09 -0600

    finishing please fix

--- added_functions.c
+++ added_functions.c
@@ -43,3 +43,33 @@ void env_end(char *buffer, char **commands, char **env)
 }
 
 /**
+* _path - Function to check and execute the command inserted
+*@commands: array sorted commands
+*@buffer: buffer from getline
+*@env: enviroment variables
+*@argv: argument vector
+*@count: number of times the prompt has been ran
+*/
+
+void _path(char **commands, char *buffer, char **env, char **argv, int count)
+{
+	struct stat fileStat2;
+	int i = 0;
+	char **directories;
+
+	directories = store_e_variables(commands[0], env);
+	while (directories[i])
+	{
+		if (stat(directories[i], &fileStat2) == 0)
+			execve(directories[i], commands, NULL);
+		i++;
+	}
+
+	/*if command not found print error*/
+	build_message(argv, commands[0], count);
+
+	free(buffer);
+	freearv(commands);
+	freearv(directories);
+	exit(EXIT_SUCCESS);
+}
