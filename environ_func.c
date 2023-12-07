61808ed3af32c97e013f395ed75094d3869160fd
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-07 15:46:27 -0600

    fixing typo and errors

--- environ_func.c
+++ environ_func.c
@@ -1,26 +1,28 @@
 #include "shell.h"
+
 /**
- * find_num - function to find the total number of directories
- * @path: path string
- * return: the number of directories in the path
- */
-unsigned int find_num(char *path)
+* find_num_dir - Function to find the total number of directories
+*@path: path string
+*Return: number of directories in the path
+*/
+unsigned int find_num_dir(char *path)
 {
-	unsigned int i = 0, flag = 0, find_num = 0;
+	unsigned int i = 0, flag = 0, num_dir = 0;
 
 	while (path[i])
 	{
 		if (path[i] != ':')
 			flag = 1;
+
 		if ((flag && path[i + 1] == ':') || (flag && path[i + 1] == '\0'))
 		{
-			find_num++;
+			num_dir++;
 			flag = 0;
 		}
 		i++;
-	}
+		}
 
-	return (find_num);
+		return (num_dir);
 }
 
 /**
@@ -38,7 +40,7 @@ char **store_e_variables(char *fir_com, char **environ)
 	int dir_length, command_length;
 
 	path_env = _getenv("PATH", environ);
-	length = find_num(path_env);
+	length = find_num_dir(path_env);
 	directories = malloc(sizeof(char *) * (length + 1));
 	if (directories == NULL)
 		return (NULL);
@@ -68,36 +70,40 @@ char **store_e_variables(char *fir_com, char **environ)
 }
 
 /**
- * _getenv - function that will get the enviroment variable
- * @name: the name of the enviroment variable
- * @env: enviroment variable
- * Return: the value of the variable
- */
-char *_getenv(const char *name, char **env)
+* _getenv - Function to get the enviroment variable
+*@name: name of the enviroment variable
+*@environ: enviroment variables
+*Return: the value associated with the variable
+*/
+
+char *_getenv(const char *name, char **environ)
 {
-	char *ev_value, *p_name;
+	char *env_value, *cp_name;
 	unsigned int i = 0, length;
 
-	length = _strlen_const(name);
+	/*find the length of the argument and malloc space for it*/
+	length =  _strlen_const(name);
 
-	p_name = malloc(sizeof(char) * length + 1);
-	if (name == NULL)
+	cp_name = malloc(sizeof(char) * length + 1);
+	if (cp_name == NULL)
 		return (NULL);
 
-	_strncpyconst(p_name, name, length);
+	/*copy the contents of the name argument to cp_name*/
+	_strncpyconst(cp_name, name, length);
 
-	ev_value = strtok(env[i], "=");
-	while (env[i])
+	/*finding the enviroment variable*/
+	env_value = strtok(environ[i], "=");
+	while (environ[i])
 	{
-		if (_strcmp(ev_value, p_name))
-		{
-			free(p_name);
-			return (ev_value);
+		if (_strcmp(env_value, cp_name))
+			{																																										env_value = strtok(NULL, "\n");
+			free(cp_name);
+			return (env_value);
 		}
 		i++;
-		ev_value = strtok(env[i], "=");
+		env_value = strtok(environ[i], "=");
 	}
-	free(p_name);
+	free(cp_name);
 	return (NULL);
 }
 
@@ -117,7 +123,7 @@ char *_strncpcommand(char *dest, char *src, char *command, int n, int c)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-
+	/*append "/" and "command" to the src*/
 	dest[i] = '/';
 	i++;
 
